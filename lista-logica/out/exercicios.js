412eefca255db0632f2e972531dbf4ae6c90c9bb
Fátima Lemes <fatimalemess@gmail.com>
2021-09-16 14:10:57 -0300

    exerciciosdefuncoes

---
+++ lista-logica/out/exercicios.js
@@ -0,0 +1,106 @@
+// Exemplos
+
+// Exercício 0A
+function soma() {
+  // escreva seu código aqui
+  const num1 = prompt('Digite o primeiro número')
+  const num2 = prompt('Digite o segundo número')
+
+  console.log(Number(num1) + Number(num2))
+}
+
+// Exercício 0B
+function imprimeMensagem() {
+  // escreva seu código aqui
+  const mensagem = prompt('Digite sua mensagem')
+
+  console.log(mensagem)
+}
+
+// ---------------------------------------------------
+// Exercícios
+
+// Exercício 1
+function calculaAreaRetangulo() {
+  // escreva seu código aqui
+  const altura = Number(prompt("Qual a altura do retângulo?"))
+  const largura = Number(prompt("Qual a largura do retângulo?))
+  const multiplicacao = altura * largura
+  console.log(multiplicacao)
+
+
+  let altura = 3
+  let largura = 5
+
+  const altura = Number(prompt("Qual a altura do retângulo?"))
+  const largura = Number(prompt("Qual a largura do retângulo?))
+
+  console.log(altura * largura)
+}
+ 
+
+
+// Exercício 2
+function imprimeIdade() {
+  // escreva seu código aqui
+}
+
+// Exercício 3
+function calculaIMC() {
+  // escreva seu código aqui
+}
+
+// Exercício 4
+function imprimeInformacoesUsuario() {
+  // escreva seu código aqui
+}
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+  // escreva seu código aqui
+  // const cores1 = ["Azul", "Amarelo", "Vermelho"]
+  // const cores2 = ["Laranja", "Roxo", "Preto"]
+  // const coresDoUsuario = prompt(`Digite 3 cores favoritas:`)
+  
+  // const cor1 = prompt('Digite a primeira cor')
+  // const cor2 = prompt('Digite a segunda cor')
+  // const cor3 = prompt('Digite a terceira cor')
+
+  // console.log([cor1, cor2, cor3])
+
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+  // escreva seu código aqui
+}
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+  // escreva seu código aqui
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+  // escreva seu código aqui
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+  // escreva seu código aqui
+}
+
+// Exercício 10
+function checaRenovacaoRG() {
+  // escreva seu código aqui
+}
+
+// Exercício 11
+function checaAnoBissexto() {
+  // escreva seu código aqui
+}
+
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+  // escreva seu código aqui
+}
